593fe3ef24a3becbf9ea6861153f5cecdfed5004
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2024-10-23 01:50:35 +0530

    (/usr/share/games/fortunes/tao) % The ancient masters were subtle, mysterious, profound, responsive. The depth of their knowledge is unfathomable. Because it is unfathomable, All we can do is describe their appearance. Watchful, like men crossing a winter stream. Alert, like men aware of danger. Courteous, like visiting guests. Yielding like ice about to melt. Simple, like uncarved blocks of wood. Hollow, like caves. Opaque, like muddy pools.
    Who can wait quietly while the mud settles?
    Who can remain still until the moment of action?
    Observers of the Tao do not seek fulfillment.
    Not seeking fulfillment, they are not swayed by desire for change.

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -520,7 +520,40 @@ local plugin_specs = {
 
   -- show and trim trailing whitespaces
   { "jdhao/whitespace.nvim", event = "VeryLazy" },
-  
+  -- ollama local AI model plugin
+  {
+  "nomnivore/ollama.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+
+  -- All the user commands added by the plugin
+  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+
+  keys = {
+    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+    {
+      "<leader>oo",
+      ":<c-u>lua require('ollama').prompt()<cr>",
+      desc = "ollama prompt",
+      mode = { "n", "v" },
+    },
+
+    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+    {
+      "<leader>oG",
+      ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+      desc = "ollama Generate Code",
+      mode = { "n", "v" },
+    },
+  },
+
+  ---@type Ollama.Config
+  opts = {
+    -- your configuration overrides
+  }
+  },
+
   -- cursor AI like plugin
   {
   "yetone/avante.nvim",
@@ -598,7 +631,7 @@ local plugin_specs = {
 
     ---@type Ollama.Config
     opts = {
-          model = "mistral",
+          model = "qwen2.5-coder",
           url = "http://127.0.0.1:11434",
           serve = {
             on_start = false,
