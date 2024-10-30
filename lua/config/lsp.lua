fb161b2ba3beed3076f89cd1f4cfa5bd15f7ceb8
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2024-10-30 15:55:24 +0530

    (/usr/share/games/fortunes/rationality) % 16 points Alejandro1 02 May 2012 07:33:52PM
        The word problem may be an insidious form of question-begging. To
        speak of the Jewish problem is to postulate that the Jews are a
        problem; it is to predict (and recommend) persecution, plunder,
        shooting, beheading, rape, and the reading of Dr. Rosenberg's prose.
        Another disadvantage of fallacious problems is that they bring about
        solutions that are equally fallacious. Pliny (Book VIII of Natural
        History) is not satisfied with the observation that dragons attack
        elephants in the summer; he ventures the hypothesis that they do it in
        order to drink the elephants' blood, which, as everyone knows, is very
        cold.
    
    -- Jorge Luis Borges, Dr. Américo Castro is Alarmed

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -111,10 +111,10 @@ end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- required by nvim-ufo
-capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true
-}
+-- capabilities.textDocument.foldingRange = {
+--     dynamicRegistration = false,
+--     lineFoldingOnly = true
+-- }
 
 local lspconfig = require("lspconfig")
 
