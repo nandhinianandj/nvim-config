affbd97320d3081a6b923a4cb26cedd82a6a0f10
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2024-11-07 22:25:08 +0530

    (/usr/share/games/fortunes/politics) % The fashionable drawing rooms of London have always been happy to accept outsiders -- if only on their own, albeit undemanding terms.  That is to say, artists, so long as they are not too talented, men of humble birth, so long as they have since amassed several million pounds, and socialists so long as they are Tories. 		-- Christopher Booker

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -555,52 +555,52 @@ local plugin_specs = {
   },
 
   -- cursor AI like plugin
-  {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  lazy = false,
-  version = false, -- set this if you want to always pull the latest change
-  opts = {
-    -- add any opts here
-  },
+  -- {
+  -- "yetone/avante.nvim",
+  -- event = "VeryLazy",
+  -- lazy = false,
+  -- version = false, -- set this if you want to always pull the latest change
+  -- opts = {
+  --   -- add any opts here
+  -- },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
-        },
-      },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-      },
-    },
-  },
+  -- build = "make",
+  -- -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  -- dependencies = {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   "stevearc/dressing.nvim",
+  --   "nvim-lua/plenary.nvim",
+  --   "MunifTanjim/nui.nvim",
+  --   --- The below dependencies are optional,
+  --   "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --   "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --   {
+  --     -- support for image pasting
+  --     "HakonHarnes/img-clip.nvim",
+  --     event = "VeryLazy",
+  --     opts = {
+  --       -- recommended settings
+  --       default = {
+  --         embed_image_as_base64 = false,
+  --         prompt_for_file_name = false,
+  --         drag_and_drop = {
+  --           insert_mode = true,
+  --         },
+  --         -- required for Windows users
+  --         use_absolute_path = true,
+  --       },
+  --     },
+  --   },
+  --   {
+  --     -- Make sure to set this up properly if you have lazy=true
+  --     'MeanderingProgrammer/render-markdown.nvim',
+  --     opts = {
+  --       file_types = { "markdown", "Avante" },
+  --     },
+  --     ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
   -- ollama caller plugin
   {
   "nomnivore/ollama.nvim",
@@ -631,7 +631,7 @@ local plugin_specs = {
 
     ---@type Ollama.Config
     opts = {
-          model = "qwen2.5-coder",
+          model = "mixtral:8x7b",
           url = "http://127.0.0.1:11434",
           serve = {
             on_start = false,
