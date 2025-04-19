397f507df5122c2d6475cffb7b7f89e5b0f4bc67
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2025-04-19 13:22:02 +0530

    (/usr/share/games/fortunes/linux) % Check it out, send me comments, and dance joyously in the streets, 		-- Linus Torvalds announcing 2.0.27

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -210,6 +210,7 @@ local plugin_specs = {
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 1 --- on start fold everything except first level folds
       vim.o.foldenable =  false
+      vim.o.nofoldenable = true 
     end,
     config = function()
       require("config.nvim_ufo")
