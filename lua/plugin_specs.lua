8149981157d167cbeeadc8465821da5bfed455d9
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2025-09-15 18:38:45 +0530

    (work) % The road to ruin is always in good repair, and the travelers pay the expense of it. 		-- Josh Billings

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -72,7 +72,69 @@ local plugin_specs = {
   { "jeetsukumaran/vim-pythonsense", ft = { "python" } },
 
   { "machakann/vim-swap", event = "VeryLazy" },
-
+  -- bookmark code files
+  {
+    "heilgar/bookmarks.nvim",
+    dependencies = {
+        "kkharji/sqlite.lua",
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        require("bookmarks").setup({
+            -- your configuration comes here
+            -- or leave empty to use defaults
+            default_mappings = true,
+            db_path = vim.fn.stdpath('data') .. '/bookmarks.db'
+        })
+        require("telescope").load_extension("bookmarks")
+    end,
+    cmd = {
+        "BookmarkAdd",
+        "BookmarkRemove",
+        "Bookmarks"
+    },
+    keys = {
+        { "<leader>ba", "<cmd>BookmarkAdd<cr>", desc = "Add Bookmark" },
+        { "<leader>br", "<cmd>BookmarkRemove<cr>", desc = "Remove Bookmark" },
+        { "<leader>bj", desc = "Jump to Next Bookmark" },
+        { "<leader>bk", desc = "Jump to Previous Bookmark" },
+        { "<leader>bl", "<cmd>Bookmarks<cr>", desc = "List Bookmarks" },
+        { "<leader>bs", desc = "Switch Bookmark List" },
+    },
+ },
+  -- Gen AI locall with ollama
+  -- Custom Parameters (with defaults)
+  {
+      "David-Kunz/gen.nvim",
+      opts = {
+          model = "mistral-nemo", -- The default model to use.
+          quit_map = "q", -- set keymap to close the response window
+          retry_map = "<c-r>", -- set keymap to re-send the current prompt
+          accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
+          host = "localhost", -- The host running the Ollama service.
+          port = "11434", -- The port on which the Ollama service is listening.
+          display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split" or "vertical-split".
+          show_prompt = false, -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
+          show_model = false, -- Displays which model you are using at the beginning of your chat session.
+          no_auto_close = false, -- Never closes the window automatically.
+          file = false, -- Write the payload to a temporary file to keep the command short.
+          hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
+          init = function(options) pcall(io.popen, "ollama serve > /dev/null 2>&1 &") end,
+          -- Function to initialize Ollama
+          command = function(options)
+              local body = {model = options.model, stream = true}
+              return "curl --silent --no-buffer -X POST http://" .. options.host .. ":" .. options.port .. "/api/chat -d $body"
+          end,
+          -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+          -- This can also be a command string.
+          -- The executed command must return a JSON object with { response, context }
+          -- (context property is optional).
+          -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+          result_filetype = "markdown", -- Configure filetype of the result buffer
+          debug = false -- Prints errors and the command which is run.
+      }
+  },
   -- IDE for Lisp
   -- 'kovisoft/slimv'
   {
